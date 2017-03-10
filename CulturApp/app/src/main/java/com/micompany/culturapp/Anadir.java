f2683ca83cbcae52d5efc53c64c36dfdbb17bf6f
Aitor <chorda.106011@e.unavarra.es>
2017-03-10 15:56:52 +0100

    inicio proyecto

---
+++ CulturApp/app/src/main/java/com/micompany/culturapp/Anadir.java
@@ -0,0 +1,13 @@
+package com.micompany.culturapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Anadir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anadir1);
+    }
+}
