162b609c3d80c60fdb431b23242a14cfdcc62860
aitor422 <aitorchorda@gmail.com>
2017-03-16 13:03:03 +0100

    Lista actividades funcionando

--- CulturApp/app/src/main/java/com/micompany/culturapp/MainActivity.java
+++ CulturApp/app/src/main/java/com/micompany/culturapp/MainActivity.java
@@ -1,11 +1,26 @@
 package com.micompany.culturapp;
 
 
+        import android.content.Context;
+        import android.content.Intent;
         import android.location.Location;
         import android.location.LocationListener;
         import android.location.LocationManager;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBarDrawerToggle;
+        import android.view.LayoutInflater;
+        import android.view.ViewGroup;
+        import android.widget.AdapterView;
+        import android.widget.ImageView;
+        import android.widget.ListView;
+        import android.widget.RelativeLayout;
+        import android.widget.TextView;
+        import android.view.View;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.widget.BaseAdapter;
+
+
 
         import com.mapbox.mapboxsdk.MapboxAccountManager;
         import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
@@ -14,10 +29,37 @@ package com.micompany.culturapp;
         import com.mapbox.mapboxsdk.maps.MapboxMap;
         import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
+        import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView = null;
     private MapboxMap mapboxMap = null;
+    ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
+    ListView mDrawerList;
+    RelativeLayout mDrawerPane;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+
+    /*
+    * Called when a particular item from the navigation drawer
+    * is selected.
+    * */
+    private void selectItemFromDrawer(int position) {
+        Intent intent;
+        switch (position) {
+            case 1:
+                intent = new Intent(this, Puntuacion.class);
+                break;
+            case 2:
+                intent = new Intent(this, Anadir.class);
+                break;
+            default:
+                intent = new Intent(this, Ajustes.class);
+                break;
+        }
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +74,26 @@ public class MainActivity extends AppCompatActivity {
             public void onMapReady(MapboxMap map) {
                 mapboxMap = map;
                 //     mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(43.26977, 5.39592), 10));
+            }
+        });
+        mNavItems.add(new NavItem("Puntuación"/*, "Meetup destination", R.drawable.ic_action_home*/));
+        mNavItems.add(new NavItem("Añadir marcador"/*, "Change your preferences", R.drawable.ic_action_settings*/));
+        mNavItems.add(new NavItem("Ajustes"/*, "Get to know about us", R.drawable.ic_action_about*/));
+
+        // DrawerLayout
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        // Populate the Navigtion Drawer with options
+        mDrawerPane = (RelativeLayout) findViewById(R.id.drawerPane);
+        mDrawerList = (ListView) findViewById(R.id.navList);
+        DrawerListAdapter adapter = new DrawerListAdapter(this, mNavItems);
+        mDrawerList.setAdapter(adapter);
+
+        // Drawer Item click listeners
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItemFromDrawer(position);
             }
         });
     }
@@ -64,6 +126,66 @@ public class MainActivity extends AppCompatActivity {
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
+    }
+    class NavItem {
+        String mTitle;
+        //String mSubtitle;
+        //int mIcon;
+
+        public NavItem(String title/*, String subtitle, int icon*/) {
+            mTitle = title;
+            //mSubtitle = subtitle;
+            //mIcon = icon;
+        }
+    }
+
+    class DrawerListAdapter extends BaseAdapter {
+
+        Context mContext;
+        ArrayList<NavItem> mNavItems;
+
+        public DrawerListAdapter(Context context, ArrayList<NavItem> navItems) {
+            mContext = context;
+            mNavItems = navItems;
+        }
+
+        @Override
+        public int getCount() {
+            return mNavItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mNavItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.drawer_item, null);
+            }
+            else {
+                view = convertView;
+            }
+
+            TextView titleView = (TextView) view.findViewById(R.id.title);
+            //TextView subtitleView = (TextView) view.findViewById(R.id.subTitle);
+            ImageView iconView = (ImageView) view.findViewById(R.id.icon);
+
+            titleView.setText( mNavItems.get(position).mTitle );
+            //subtitleView.setText( mNavItems.get(position).mSubtitle );
+            //iconView.setImageResource(mNavItems.get(position).mIcon);
+
+            return view;
+        }
     }
 
 }
