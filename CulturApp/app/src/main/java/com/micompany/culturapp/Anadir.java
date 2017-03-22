860efdce3abff056c19aaeffdebce65921c226ec
Aitor Chordá <Aitor Chord>
2017-03-22 22:34:20 +0100

    Casi acabadas las vistas de añadir

--- CulturApp/app/src/main/java/com/micompany/culturapp/Anadir.java
+++ CulturApp/app/src/main/java/com/micompany/culturapp/Anadir.java
@@ -1,6 +1,8 @@
 package com.micompany.culturapp;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,15 +10,57 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationListener;
+import com.mapbox.mapboxsdk.location.LocationServices;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
 public class Anadir extends AppCompatActivity {
     private ImageButton flecha;
+    Marker marker;
     private FloatingActionButton alante;
     public static Anadir anadir1;
+    private LocationServices locationServices;
+
+    private MapView mapView = null;
+    private MapboxMap mapboxMap = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        MapboxAccountManager.start(this, getString(R.string.access_token));
         super.onCreate(savedInstanceState);
         anadir1 = this;
         setContentView(R.layout.activity_anadir1);
+        mapView = (MapView)findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap map) {
+                mapboxMap = map;
+                locationServices = LocationServices.getLocationServices(Anadir.this);
+                final Location lastLocation = locationServices.getLastLocation();
+                final MarkerViewOptions markerViewOptions = new MarkerViewOptions()
+                        .position(new LatLng(lastLocation));
+
+                mapboxMap.addMarker(markerViewOptions);
+                mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastLocation), 16));
+                marker = mapboxMap.getMarkers().get(0);
+                mapboxMap.setOnScrollListener(new MapboxMap.OnScrollListener() {
+                    @Override
+                    public void onScroll() {
+                        marker.setPosition(mapboxMap.getCameraPosition().target);
+                        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mapboxMap.getCameraPosition().target), 16));
+                    }
+                });
+
+            }
+        });
         flecha = (ImageButton) findViewById(R.id.flecha);
         alante = (FloatingActionButton) findViewById(R.id.forward);
         flecha.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +74,12 @@ public class Anadir extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Anadir.this, Anadir2.class);
+                Double l1=marker.getPosition().getLatitude();
+                Double l2=marker.getPosition().getLongitude();
+                String coordl1 = l1.toString();
+                String coordl2 = l2.toString();
+                intent.putExtra("LATITUD", coordl1);
+                intent.putExtra("LONGITUD", coordl2);
                 startActivity(intent);
             }
         });
@@ -39,4 +89,36 @@ public class Anadir extends AppCompatActivity {
     private void killActivity() {
         finish();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+
 }
--- CulturApp/app/src/main/java/com/micompany/culturapp/Anadir2.java
+++ CulturApp/app/src/main/java/com/micompany/culturapp/Anadir2.java
@@ -1,22 +1,53 @@
 package com.micompany.culturapp;
 
 import android.content.Intent;
+
+import android.graphics.Bitmap;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Anadir2 extends AppCompatActivity {
     private ImageButton flecha;
     private FloatingActionButton alante;
+    private Button boton;
+    private ImageView imagen;
+    private TextView nombre;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private String mCurrentPhotoPath;
+    private Bitmap imageBitmap;
+
+
+
     public static Anadir2 anadir2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         anadir2 = this;
         setContentView(R.layout.activity_anadir2);
+        Intent intent = getIntent();
+        final String latitud = intent.getStringExtra("LATITUD");
+        final String longitud = intent.getStringExtra("LONGITUD");
+
+        nombre = (TextView) findViewById(R.id.nombre);
         flecha = (ImageButton) findViewById(R.id.flecha);
+        boton = (Button) findViewById(R.id.botonimagen);
+        imagen = (ImageView) findViewById(R.id.imagen);
 
         alante = (FloatingActionButton) findViewById(R.id.forward);
         flecha.setOnClickListener(new View.OnClickListener() {
@@ -25,11 +56,22 @@ public class Anadir2 extends AppCompatActivity {
                 killActivity();
             }
         });
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FromCamera();
+            }
+        });
 
         alante.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Anadir2.this, Anadir3.class);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                intent.putExtra("NOMBRE", nombre.getText());
+                intent.putExtra("LATITUD", latitud);
+                intent.putExtra("LONGITUD", longitud);
+                intent.putExtra("IMAGEN", imageBitmap);
                 startActivity(intent);
             }
         });
@@ -38,5 +80,21 @@ public class Anadir2 extends AppCompatActivity {
 
     private void killActivity() {
         finish();
+    }
+
+    private void FromCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            imagen.setImageBitmap(imageBitmap);
+        }
     }
 }
