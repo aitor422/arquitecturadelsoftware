63b7c7b0fc3cbd078e40b91293a6fac12be0b26d
irati <irati@debian.midomino.es>
2017-03-24 19:54:40 +0100

    Añadir3 maomeno done

--- CulturApp/app/src/main/java/com/micompany/culturapp/Anadir3.java
+++ CulturApp/app/src/main/java/com/micompany/culturapp/Anadir3.java
@@ -3,13 +3,25 @@ package com.micompany.culturapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Anadir3 extends AppCompatActivity {
-    private ImageButton flecha;
-    private FloatingActionButton okay;
 
+    private ImageButton flecha, mas, menos;
+    private FloatingActionButton okay;
+    List<EditText> opciones;
+    List<RadioButton> botones;
+    private RadioGroup radioGroup;
+    private int numOpciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,10 +47,64 @@ public class Anadir3 extends AppCompatActivity {
                 killActivity();
             }
         });
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        opciones = new ArrayList<EditText>();
+        opciones.add( (EditText) findViewById(R.id.editText0));
+        opciones.add( (EditText) findViewById(R.id.editText1));
+        opciones.add( (EditText) findViewById(R.id.editText2));
+        opciones.add( (EditText) findViewById(R.id.editText3));
+        botones = new ArrayList<RadioButton>();
+        botones.add( (RadioButton) findViewById(R.id.radioButton0));
+        botones.add( (RadioButton) findViewById(R.id.radioButton1));
+        botones.add( (RadioButton) findViewById(R.id.radioButton2));
+        botones.add( (RadioButton) findViewById(R.id.radioButton3));
+        numOpciones=2;
+
+        //Añadir accion: Añadir opcion al darle a mas
+        mas = (ImageButton) findViewById(R.id.mas);
+        mas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numOpciones==2){
+                    botones.get(2).setVisibility(View.VISIBLE);
+                    opciones.get(2).setVisibility(View.VISIBLE);
+                    numOpciones++;
+                }else if (numOpciones==3){
+                    botones.get(3).setVisibility(View.VISIBLE);
+                    opciones.get(3).setVisibility(View.VISIBLE);
+                    numOpciones++;
+                }
+            }
+        });
+        //Añadir accion: Eliminar opcion al darle a menos
+        menos = (ImageButton) findViewById(R.id.menos);
+        menos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (numOpciones==3){
+                    botones.get(2).setVisibility(View.GONE);
+                    opciones.get(2).setVisibility(View.GONE);
+                    if (botones.get(2).isChecked()){
+                        radioGroup.check(botones.get(1).getId());
+                    }
+                    numOpciones--;
+                }else if (numOpciones==4){
+                    botones.get(3).setVisibility(View.GONE);
+                    opciones.get(3).setVisibility(View.GONE);
+                    if (botones.get(3).isChecked()){
+                        radioGroup.check(botones.get(2).getId());
+                    }
+                    numOpciones--;
+                }
+            }
+        });
     }
 
     //funcion finalizar actividad
     private void killActivity() {
         finish();
     }
+
+
 }
---
+++ CulturApp/app/src/main/java/com/micompany/culturapp/Contestar.java
@@ -0,0 +1,8 @@
+package com.micompany.culturapp;
+
+/**
+ * Created by irati on 24/03/17.
+ */
+
+public class Contestar {
+}
